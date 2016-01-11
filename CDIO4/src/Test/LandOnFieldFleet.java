37a0b0681fd0e727ba14196c029b1e8d3eb80031
Alexander <Alexander@10.16.172.119>
2016-01-11 10:33:16 +0100

    trying to merge

--- CDIO4/src/Test/LandOnFieldFleet.java
+++ CDIO4/src/Test/LandOnFieldFleet.java
@@ -1,70 +1,67 @@
-//package Test;
-//
-//import static org.junit.Assert.*;
-//
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import entities.fields.Brewery;
-//import entities.fields.ownable.Fleet;
-//import entities.fields.ownable.LaborCamp;
-//=======
-//import Fields.Fleet;
-//import game.Player;
-//
-//public class LandOnFieldFleet
-//{
-//
-//	private Player p1;
-//	private Player p2;
-//	private Fleet fleet1;
-//	private Fleet fleet2;
-//	private Fleet fleet3;
-//	private Fleet fleet4;
-//
-//	@Before
-//
-//	public void setUp() throws Exception
-//	{
-//
-//		this.p1 = new Player("Henrik", 30000, true);
-//		this.p2 = new Player("Bob", 10000, true);
-//		this.fleet1 = new Fleet("Sea Grover", 10);
-//		this.fleet2 = new Fleet("Second Sail", 6);
-//		this.fleet3 = new Fleet("Privateer Armada", 20);
-//		this.fleet4 = new Fleet("The Buccaneers", 16);
-//
-//	}
-//
-//	@Test
-//	public void test()
-//	{
-//
-//	}
-//
-//	@Test
-//	public void testfleetsowned()
-//	{
-//		fleet1.landOnField(p1);
-//		fleet1.landOnField(p2);
-//		int expected = 9500;
-//		Assert.assertEquals(expected, p2.getAccount().getBalance());
-//
-//		fleet2.landOnField(p1);
-//		fleet2.landOnField(p2);
-//		expected = 8500;
-//		Assert.assertEquals(expected, p2.getAccount().getBalance());
-//
-//		fleet3.landOnField(p1);
-//		fleet3.landOnField(p2);
-//		expected = 6500;
-//		Assert.assertEquals(expected, p2.getAccount().getBalance());
-//
-//		fleet4.landOnField(p1);
-//		fleet4.landOnField(p2);
-//		expected = 2500;
-//		Assert.assertEquals(expected, p2.getAccount().getBalance());
-//
-//	}
-//}
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import entities.fields.ownable.Fleet;
+import game.Player;
+
+public class LandOnFieldFleet
+{
+
+	private Player p1;
+	private Player p2;
+	private Fleet fleet1;
+	private Fleet fleet2;
+	private Fleet fleet3;
+	private Fleet fleet4;
+
+	@Before
+
+	public void setUp() throws Exception
+	{
+
+		this.p1 = new Player("Henrik", 30000, true);
+		this.p2 = new Player("Bob", 10000, true);
+		this.fleet1 = new Fleet("Sea Grover", 10);
+		this.fleet2 = new Fleet("Second Sail", 6);
+		this.fleet3 = new Fleet("Privateer Armada", 20);
+		this.fleet4 = new Fleet("The Buccaneers", 16);
+
+	}
+
+	@Test
+	public void test()
+	{
+
+	}
+
+	@Test
+	public void testfleetsowned()
+	{
+		fleet1.landOnField(p1);
+		fleet1.landOnField(p2);
+		int expected = 9500;
+		Assert.assertEquals(expected, p2.getAccount().getBalance());
+
+		fleet2.landOnField(p1);
+		fleet2.landOnField(p2);
+		expected = 8500;
+		Assert.assertEquals(expected, p2.getAccount().getBalance());
+
+		fleet3.landOnField(p1);
+		fleet3.landOnField(p2);
+		expected = 6500;
+		Assert.assertEquals(expected, p2.getAccount().getBalance());
+
+		fleet4.landOnField(p1);
+		fleet4.landOnField(p2);
+		expected = 2500;
+		Assert.assertEquals(expected, p2.getAccount().getBalance());
+
+	}
+}
