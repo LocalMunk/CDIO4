f62028e96d1728e02ca39e87d1c2d3d7a3575d24
Gundel <Gundel@192.168.0.12>
2016-01-11 20:12:40 +0100

    Fixed tests PlayerJailed and LandOnFieldFleet.

--- CDIO4/src/Test/LandOnFieldFleet.java
+++ CDIO4/src/Test/LandOnFieldFleet.java
@@ -2,6 +2,7 @@ package Test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,7 +14,7 @@ import game.Player;
 public class LandOnFieldFleet
 {
 
-	private Player p1;
+	private Player p1 = new Player("Henrik", 30000, true);;
 	private Player p2;
 	private Fleet fleet1;
 	private Fleet fleet2;
@@ -25,43 +26,58 @@ public class LandOnFieldFleet
 	public void setUp() throws Exception
 	{
 
-//		this.p1 = new Player("Henrik", 30000, true);
-//		this.p2 = new Player("Bob", 10000, true);
-//		this.fleet1 = new Fleet("Sea Grover", 10);
-//		this.fleet2 = new Fleet("Second Sail", 6);
-//		this.fleet3 = new Fleet("Privateer Armada", 20);
-//		this.fleet4 = new Fleet("The Buccaneers", 16);
+		int[] fleetRent = new int[] {400, 500, 600, 700};
+		this.p2 = new Player("Bob", 1000, true);
+		this.fleet1 = new Fleet("Første","Fleet",14,4000 , fleetRent);
+		this.fleet2= new Fleet("Anden","Fleet",15,4000 , fleetRent);
+		this.fleet3 = new Fleet("Tredje","Fleet",16,4000 , fleetRent);
+		this.fleet4 = new Fleet("Fjerde","Fleet",17,4000 , fleetRent);
 
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		p2 = null;
 	}
 
 	@Test
-	public void test()
-	{
+	public void testLandOnFleet1() { //Test that a player pays 400 gold for landing on a single owned fleet
 
-	}
-
-	@Test
-	public void testfleetsowned()
-	{
-		fleet1.landOnField(p1);
+		fleet1.setOwned(true);
+		fleet1.setOwner(p1);
+		p1.addFleetsOwned();
 		fleet1.landOnField(p2);
-		int expected = 9500;
-		Assert.assertEquals(expected, p2.getAccount().getBalance());
-
-		fleet2.landOnField(p1);
+		Assert.assertEquals(600, p2.getAccount().getBalance());
+		
+	}
+	@Test
+	public void testLandOnFleet2() { //Test rent is 500 gold if 2 fleets are owned
+		fleet2.setOwned(true);
+		fleet2.setOwner(p1);
+		p1.addFleetsOwned();
+		p1.addFleetsOwned();
 		fleet2.landOnField(p2);
-		expected = 8500;
-		Assert.assertEquals(expected, p2.getAccount().getBalance());
-
-		fleet3.landOnField(p1);
+		Assert.assertEquals(500, p2.getAccount().getBalance());
+	}
+	@Test
+	public void testLandOnFleet3() { //Test rent is 600 gold if 3 fleets are owned
+		fleet3.setOwned(true);
+		fleet3.setOwner(p1);
+		p1.addFleetsOwned();
+		p1.addFleetsOwned();
+		p1.addFleetsOwned();
 		fleet3.landOnField(p2);
-		expected = 6500;
-		Assert.assertEquals(expected, p2.getAccount().getBalance());
-
-		fleet4.landOnField(p1);
+		Assert.assertEquals(400, p2.getAccount().getBalance());
+	}
+	@Test
+	public void testLandOnFleet4() { //Test rent is 700 gold if 4 fleets are owned
+		fleet4.setOwned(true);
+		fleet4.setOwner(p1);
+		p1.addFleetsOwned();
+		p1.addFleetsOwned();
+		p1.addFleetsOwned();
+		p1.addFleetsOwned();
 		fleet4.landOnField(p2);
-		expected = 2500;
-		Assert.assertEquals(expected, p2.getAccount().getBalance());
-
+		Assert.assertEquals(300, p2.getAccount().getBalance());
 	}
 }
--- CDIO4/src/Test/PlayerJailed.java
+++ CDIO4/src/Test/PlayerJailed.java
@@ -16,62 +16,67 @@ import desktop_resources.GUI;
 public class PlayerJailed {
 
 	private Player Rasmus;
-	private Player SÃ¸ren;
-	private Territory T1;
-	private Brewery B1;
-	private Fleet F1;
+	private Player Søren;
 	private FieldCollection fields;
 
 	@Before
 	public void setUp() throws Exception {
 		fields = new FieldCollection();
 		fields.initialize();
-		this.Rasmus = new Player("Rasmus", 2000, true);
-		this.SÃ¸ren = new Player("SÃ¸ren", 20000, true);
-		GUI.addPlayer("Rasmus", 2000);
-		GUI.addPlayer("SÃ¸ren", 20000);
+		this.Rasmus = new Player("Rasmus", 20000, true);
+		this.Søren = new Player("Søren", 20000, true);
+		GUI.addPlayer("Rasmus", 20000);
+		GUI.addPlayer("Søren", 20000);
 
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		fields = null;
 		this.Rasmus = null;
-		this.SÃ¸ren = null;
+		this.Søren = null;
+		GUI.close();
+		fields = null;
+		
 
 	}
 
+
+	
+	
+	
+	
+	
+	
 	@Test
 	public void testBrewery() {
-		fields.getField(28).landOnField(SÃ¸ren);
-		System.out.println(SÃ¸ren.getAccount().getBalance());
-		SÃ¸ren.setJailed(true);
+		fields.getField(28).landOnField(Søren);
+		Søren.setJailed(true);
 		fields.getField(28).landOnField(Rasmus);
-		assertEquals(17000, SÃ¸ren.getAccount().getBalance());
-		assertEquals(2000, Rasmus.getAccount().getBalance());
+		GUI.setCar(29, this.Rasmus.getName());
+		assertEquals(17000, Søren.getAccount().getBalance());
+		assertEquals(20000, Rasmus.getAccount().getBalance());
 
 	}
 
 	@Test
 	public void testTerritory() {
-		fields.getField(31).landOnField(SÃ¸ren);
-		SÃ¸ren.setJailed(true);
-		System.out.println(SÃ¸ren.getAccount().getBalance());
+		fields.getField(31).landOnField(Søren);
+		Søren.setJailed(true);
 		fields.getField(31).landOnField(Rasmus);
-		GUI.setCar(31, "Rasmus");
-		assertEquals(14000, SÃ¸ren.getAccount().getBalance());
-		assertEquals(2000, Rasmus.getAccount().getBalance());
+		GUI.setCar(32, this.Rasmus.getName());
+		assertEquals(14000, Søren.getAccount().getBalance());
+		assertEquals(20000, Rasmus.getAccount().getBalance());
 
 	}
 
 	@Test
 	public void testFleet() {
-		fields.getField(14).landOnField(SÃ¸ren);
-		System.out.println(SÃ¸ren.getAccount().getBalance());
-		SÃ¸ren.setJailed(true);
+		fields.getField(14).landOnField(Søren);
+		Søren.setJailed(true);
 		fields.getField(14).landOnField(Rasmus);
-		assertEquals(16000, SÃ¸ren.getAccount().getBalance());
-		assertEquals(2000, Rasmus.getAccount().getBalance());
+		GUI.setCar(15, this.Rasmus.getName());
+		assertEquals(16000, Søren.getAccount().getBalance());
+		assertEquals(20000, Rasmus.getAccount().getBalance());
 
 	}
 
