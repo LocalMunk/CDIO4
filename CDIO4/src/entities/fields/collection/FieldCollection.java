3da2e85ff9c423bf8b2b194c39ff0a5cbc713324
Gundel <Gundel@192.168.0.12>
2016-01-11 20:52:28 +0100

    Changed FieldCollection (FieldID/Index) to spread out Chancefields. Added seperate colours to match different collections of fields.

--- CDIO4/src/entities/fields/collection/FieldCollection.java
+++ CDIO4/src/entities/fields/collection/FieldCollection.java
@@ -38,47 +38,48 @@ public class FieldCollection
 		//Chance cards
 		fields[2] = new ChanceField("Try your luck","IS your luck shit?",2);
 		fields[7] = new ChanceField("Try your luck","Is your luck shit?",7);
-		fields[18] = new ChanceField("Try your luck","ChanceCard",18);
-		fields[17] = new ChanceField("Try your luck","Street", 17);
+		fields[15] = new ChanceField("Try your luck","Street", 15);
+		fields[24] = new ChanceField("Try your luck","ChanceCard",24);
 		fields[33] = new ChanceField("Try your luck", "is your luck shit?", 33);
 		fields[36] = new ChanceField("Try your luck", "is your luck shit?", 36);
 		
 		//Territory
-		fields[1] 	= new Territory("Rï¿½dovrevej",			"Street", "Blue",1,1200, new int[] {50,250,750,2250,4000,6000} );
+		fields[1] 	= new Territory("Rødovrevej",			"Street", "Blue",1,1200, new int[] {50,250,750,2250,4000,6000} );
 		fields[3] 	= new Territory ("Hvidovrevej",			"Street", "Blue",3,1200, new int[] {50,250,750,2250,4000,6000} );
 		fields[6] 	= new Territory ("Roskildevej",			"Street", "Salmon",6, 2000 ,new int[]{100,600,1800,5400,8000,11000});
 		fields[8] 	= new Territory ("Valby langgade",		"Street", "Salmon",8,2000,new int[]{100,600,1800,5400,8000,11000} );
-		fields[9] 	= new Territory ("Alle gadï¿½",			"Street", "Salmon",9,2400 ,new int[]{150,800,2000,6000,9000,12000});
-		fields[10]	= new Territory ("Frederisksberg allï¿½",	"Street", "Green",10,2800 ,new int[]{200,1000,3000,9000,12500,15000});
-		fields[12]	= new Territory ("Bï¿½lwsvej",			"Street", "Green",12,2800 ,new int[]{200,1000,3000,9000,12500,15000});
+		fields[9] 	= new Territory ("Alle gadé",			"Street", "Salmon",9,2400 ,new int[]{150,800,2000,6000,9000,12000});
+		fields[11]	= new Territory ("Frederisksberg allé",	"Street", "Green",11,2800 ,new int[]{200,1000,3000,9000,12500,15000});
+		fields[12]	= new Territory ("Bülwsvej",			"Street", "Green",12,2800 ,new int[]{200,1000,3000,9000,12500,15000});
 		fields[13] 	= new Territory ("Gammelkonge vej", 	"Street", "Green",13,3200 ,new int[]{250,1250,3750,1000,14000,18000});
-		fields[15] 	= new Territory ("Bernsstoffvej",		"Street", "Grey",15,3600 ,new int[]{300,1400,4000,11000,15000,19000});
-		fields[21] 	= new Territory ("Hellerupvej",			"Street", "Grey",21,3600 ,new int[]{300,1400,4000,11000,15000,1900});
-		fields[19] 	= new Territory ("Strandvejen",			"Street", "Grey",19,4000 ,new int[]{350,600,4400,12000,16000,19000});
-		fields[22] 	= new Territory ("Trianglen",			"Street", "Red",22,4400 ,new int[]{300,1800,5000,14000,17500,21000});
-		fields[23] 	= new Territory("ï¿½sterbrogade", 		"Street", "Red", 23, 4400, new int[]{350,1800,5000,14000,17500,21000});
-		fields[24] 	= new Territory("Grï¿½nningen", 			"Street", "Red", 24, 4800, new int[]{300,1800,5000,14000,17500,21000});
+		fields[17] 	= new Territory ("Bernsstoffvej",		"Street", "Grey",17,3600 ,new int[]{300,1400,4000,11000,15000,19000});
+		fields[18] 	= new Territory ("Strandvejen",			"Street", "Grey",18,4000 ,new int[]{350,600,4400,12000,16000,19000});
+		fields[19] 	= new Territory ("Hellerupvej",			"Street", "Grey",19,3600 ,new int[]{300,1400,4000,11000,15000,1900});
+		fields[21] 	= new Territory ("Trianglen",			"Street", "Red",21,4400 ,new int[]{300,1800,5000,14000,17500,21000});
+		fields[22] 	= new Territory("Østerbrogade", 		"Street", "Red", 22, 4400, new int[]{350,1800,5000,14000,17500,21000});
+		fields[23] 	= new Territory("Grønningen", 			"Street", "Red", 23, 4800, new int[]{300,1800,5000,14000,17500,21000});
 		fields[26] 	= new Territory("Bredgade", 			"Street", "White", 26, 5200,new int[]{450,2200,6600,16000,19500,23000});
 		fields[27] 	= new Territory("Kgs. Nytorv", 			"Street", "White", 27, 5200, new int[]{450,2200,6600,16000,19500,23000});
-		fields[29] 	= new Territory("ï¿½stergade", 			"Street", "White", 29, 5600,new int[]{500,2400,7200,17000,20500,24000});
+		fields[29] 	= new Territory("Østergade", 			"Street", "White", 29, 5600,new int[]{500,2400,7200,17000,20500,24000});
 		fields[31] 	= new Territory("Amagertorv", 			"Street", "Yellow", 31, 6000,new int[]{550,2600,7800,18000,22000,25000});
 		fields[32] 	= new Territory("Vimmelskaftet", 		"Street", "Yellow", 32, 6000,new int[]{550,2600,7800,18000,22000,25000});
 		fields[35] 	= new Territory("Nygade",				"Street", "Yellow", 35, 6400,new int[]{600,3000,9000,20000,24000,28000});
+		fields[38] 	= new Territory("Rådhuspladsen", 		"Street", "Purple", 38, 8000,new int[]{1000,4000,12000,28000,34000,40000});
 		fields[39] 	= new Territory("Frederiksberggade", 	"Street", "Purple", 39, 7000,new int[]{700,3500,10000,22000,26000,30000});
-		fields[38] 	= new Territory("Rï¿½dhuspladsen", 		"Street", "Purple", 38, 8000,new int[]{1000,4000,12000,28000,34000,40000});
+		
 		
 		//Tax
 		fields[4] = new Tax("Pay Tax","You need to pay taxes",4,200,10);
 		fields[37] = new Tax("Skat", "Pay your damned taxes", 37, 200, 10);
 		
 		//Fleet
-		fields[5] = new Fleet ("ï¿½resundsredderiet","Fleet",5,4000, fleetRent);
+		fields[5] = new Fleet ("Øresundsredderiet","Fleet",5,4000, fleetRent);
 		fields[14] = new Fleet("DFDS","Fleet",14,4000 , fleetRent);
-		fields[25] = new Fleet("ï¿½.S. redderiet", "Fleet", 25, 4000, fleetRent);
+		fields[25] = new Fleet("A/S. redderiet", "Fleet", 25, 4000, fleetRent);
 		fields[34] = new Fleet("Bornholm", "Fleet", 34, 4000, fleetRent);
 		
 		//Jail
-		fields[11] = new Jail ("Jail","Your jailed bitch",11);
+		fields[10] = new Jail ("Jail","Your jailed bitch",10);
 		fields[30] = new GoToJail("Go to jail", "go to jail", 30);
 		
 		//Brewery
--- CDIO4/src/game/controllers/GameBoard.java
+++ CDIO4/src/game/controllers/GameBoard.java
@@ -5,13 +5,11 @@ import desktop_resources.GUI;
 import desktop_fields.Field;
 import desktop_fields.Ownable;
 
-
 import entities.ChanceCards.ChanceCard;
 
 import java.awt.Color;
 
-public class GameBoard
-{
+public class GameBoard {
 	private ChanceCard[] chances;
 	private static Field[] fields;
 
@@ -19,89 +17,79 @@ public class GameBoard
 	 * builds the gameboard, the first part builds the GUI board, and the second
 	 * builds the code board.
 	 */
-	public static void initializeGUI(entities.fields.abstracts.Field[] fields)
-	{
+	public static void initializeGUI(entities.fields.abstracts.Field[] fields) {
 		Field[] fieldsGUI = new Field[fields.length];
 		int i = 0;
-		for(entities.fields.abstracts.Field item : fields)
-		{
-			Color  background = Color.lightGray;
-		    String subtext    = "Territory";
-		    
-		    if(item instanceof entities.fields.Refuge)
-		    {
-		        background = Color.green;
-		        subtext    = "Refuge";
-		    }
-		    
-		    else if(item instanceof entities.fields.Tax)
-	        {
-	            background = Color.magenta;
-	            subtext    = "Tax";
-	        }
-		    
-		    else if(item instanceof entities.fields.GoToJail)
-	        {
-	            background = Color.lightGray;
-	            subtext    = "Go to jail";
-	        }
-		    
-		    else if(item instanceof entities.fields.Jail)
-	        {
-	            background = Color.gray;
-	            subtext    = "Jail";
-	        }
-		    
-//		    else if(item instanceof entities.fields)
-//	        {
-//	            background = Color.yellow;
-//	            subtext    = "Chance card";
-//	        }
-		    else if(item instanceof entities.fields.FreeParking)
-	        {
-	            background = Color.lightGray;
-	            subtext    = "Free parking";
-	        }
-		    else if(item instanceof entities.fields.StartField)
-	        {
-	            background = Color.red;
-	            subtext    = "Start";
-	        }
-		    
-		    
-		    //Ownable fields
-		    else if(item instanceof entities.fields.ownable.Fleet)
-		    {
-		        background = Color.cyan;
-		        subtext    = "Fleet";
-		    }
-		    
-		    else if(item instanceof entities.fields.ownable.Brewery)
-		    {
-		        background = Color.orange;
-		        subtext    = "Brewery";
-		    }
-		    
-		    else if(item instanceof entities.fields.ownable.Territory)
-		    {
-		    	background = Color.blue;
-		        subtext    = "Territory";
-		    }
-		    
-		    /*
-		     * The index is reduced by one to fit into
-		     * the array.
-		     */
-		    fieldsGUI[i] = new Street.Builder()
-	            .setTitle(item.getName())
-	            .setSubText(subtext)
-	            .setDescription(item.getDescription())
-	            .setBgColor(background)
-	            .build();
-			
-		    i++;
+		for (entities.fields.abstracts.Field item : fields) {
+			Color background = Color.lightGray;
+			String subtext = "Territory";
+
+			if (i == 1 || i == 3) {
+				background = Color.orange;
+				subtext = "Territory";
+			} else if (i == 6 || i == 8 || i == 9) {
+				background = Color.pink;
+				subtext = "Territory";
+			} else if (i == 11 || i == 12 || i == 13) {
+				background = Color.green;
+				subtext = "Territory";
+			} else if (i == 17 || i == 18 || i == 19) {
+				background = Color.blue;
+				subtext = "Territory";
+			} else if (i == 21 || i == 22 || i == 23) {
+				background = Color.red;
+				subtext = "Territory";
+			} else if (i == 26 || i == 27 || i == 29) {
+				background = Color.white;
+				subtext = "Territory";
+			} else if (i == 31 || i == 32 || i == 35) {
+				background = Color.yellow.brighter();
+				subtext = "Territory";
+			} else if (i == 38 || i == 39) {
+				background = Color.lightGray;
+				subtext = "Territory";
+			} else if (item instanceof entities.fields.Tax) {
+				background = Color.magenta.darker();
+				subtext = "Tax";
+			}
+
+			else if (item instanceof entities.fields.GoToJail) {
+				background = Color.darkGray;
+				subtext = "Go to jail";
+			} else if (item instanceof entities.fields.Jail) {
+				background = Color.darkGray;
+				subtext = "Jail";
+			} else if (item instanceof entities.fields.FreeParking) {
+				background = Color.darkGray;
+				subtext = "Free Parking!";
+			} else if (item instanceof entities.fields.ChanceField) {
+				background = Color.cyan;
+				subtext = "Chance Card";
+			} else if (item instanceof entities.fields.StartField) {
+				background = Color.red;
+				subtext = "Start";
+			}
+
+			// Ownable fields
+			else if (item instanceof entities.fields.ownable.Fleet) {
+				background = Color.red.darker();
+				subtext = "Fleet";
+			}
+
+			else if (item instanceof entities.fields.ownable.Brewery) {
+				background = Color.orange.darker();
+				subtext = "Brewery";
+			}
+
+			/*
+			 * The index is reduced by one to fit into the array.
+			 */
+			fieldsGUI[i] = new Street.Builder().setTitle(item.getName()).setSubText(subtext)
+					.setDescription(item.getDescription()).setBgColor(background).build();
+
+			i++;
 		}
-			GUI.create(fieldsGUI);
-	
+		GUI.create(fieldsGUI);
+
 	}
 }
